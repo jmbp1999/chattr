8695aab9a7febd0ffef55c2b8356b410a73c3fe4
jmbp1999 <pamudithaj98@gmail.com>
2022-07-13 19:57:57 +0530

    added set avatar functionality

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,8 @@
-const { login, register } = require('../controllers/userController');
+const { login, register, setAvatar } = require('../controllers/userController');
 const router = require('express').Router();
 
 router.post('/login', login);
 router.post('/register', register);
+router.post('/setavatar/:id', setAvatar);
 
 module.exports = router;
